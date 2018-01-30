10671b2d803681c64bca949b510b52922b900bc0
Sebastien Carlier <s.carlier@epixode.fr>
2018-01-30 13:12:45 +0000

    add Hint request button

--- src/workspace_bundle.js
+++ src/workspace_bundle.js
@@ -1,16 +1,31 @@
 
 import React from 'react';
+import {Button} from 'react-bootstrap';
 import {connect} from 'react-redux';
 import {range} from 'range';
 
 function WorkspaceSelector (state) {
-  const {views: {CipheredText, SelectedText, FrequencyAnalysis, Rotor, SchedulingControls, DecipheredText}, rotors} = state;
-  return {CipheredText, SelectedText, FrequencyAnalysis, Rotor, SchedulingControls, DecipheredText, nbRotors: rotors.length};
+  const {
+    views: {CipheredText, SelectedText, FrequencyAnalysis, Rotor, SchedulingControls, DecipheredText},
+    actions: {requestHint},
+    rotors, editing
+  } = state;
+  let hintRequest = null;
+  if (typeof editing.rotorIndex === 'number') {
+    const editingCell = rotors[editing.rotorIndex].cells[editing.cellRank];
+    if (!editingCell.hint && !editingCell.locked) {
+      hintRequest = editing;
+    }
+  }
+  return {
+    CipheredText, SelectedText, FrequencyAnalysis, Rotor, SchedulingControls, DecipheredText,
+    requestHint, hintRequest, nbRotors: rotors.length
+  };
 }
 
 class Workspace extends React.PureComponent {
   render () {
-    const {CipheredText, SelectedText, FrequencyAnalysis, Rotor, SchedulingControls, DecipheredText, nbRotors} = this.props;
+    const {CipheredText, SelectedText, FrequencyAnalysis, Rotor, SchedulingControls, DecipheredText, nbRotors, hintRequest} = this.props;
     return (
       <div>
         <h2>{"Message chiffré"}</h2>
@@ -21,12 +36,20 @@ class Workspace extends React.PureComponent {
         <FrequencyAnalysis/>
         <h2>{`Rotor${nbRotors > 1 ? 's' : ''} de déchiffrement`}</h2>
         {range(0, nbRotors).map(index => <Rotor key={index} index={index}/>)}
-        <SchedulingControls/>
+        <div>
+          {hintRequest &&
+            <Button style={{float: 'left'}} onClick={this.requestHint}>{`Indice`}</Button>}
+          <SchedulingControls/>
+        </div>
         <h2>{"Texte déchiffré"}</h2>
         <DecipheredText/>
       </div>
     );
   }
+  requestHint = () => {
+    const {dispatch, requestHint, hintRequest} = this.props;
+    dispatch({type: requestHint, payload: {request: hintRequest}});
+  };
 }
 
 export default {
